31265db2909f30931f74479c58d84eb7e0aff1ab
NunoF <n.fernandes.contact@gmail.com>
2025-07-19 09:30:05 +0200

    feat(clients): add base client DTO for client management
    Introduce BaseClientDto to standardize client data structure with validation decorators. Includes essential fields like id, name, email, and optional fields for phone, address, and client type. Uses class-validator for input validation and class-transformer for type conversion.

---
+++ src/clients/dto/base-client.dto.ts
@@ -0,0 +1,56 @@
+import { GetUsersDto } from '../../user/dto/get-users.dto';
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsPhoneNumber,
+  IsEnum,
+  IsDate,
+} from 'class-validator';
+
+export class BaseClientDto {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsEmail()
+  email2: string;
+
+  @IsOptional()
+  @IsString()
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsOptional()
+  @IsString()
+  address: string;
+
+  @IsOptional()
+  @IsString()
+  siret: string;
+
+  @IsOptional()
+  @IsEnum(['PROFESSIONAL', 'PERSONAL'])
+  type: string[];
+
+  @IsDate()
+  @Type(() => Date)
+  createdAt: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  updatedAt: Date;
+
+  user: GetUsersDto;
+}
