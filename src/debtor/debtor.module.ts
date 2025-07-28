b56896253c95ba413e66c2910d838ad75eb2d05b
NunoF <n.fernandes.contact@gmail.com>
2025-07-28 04:00:02 +0200

    feat(debtor): add CRUD services and integrate with module
    Implement core debtor services for create, read, update and delete operations
    Refactor DebtorModule and DebtorService to use new service classes

--- src/debtor/debtor.module.ts
+++ src/debtor/debtor.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DebtorController } from './debtor.controller';
 import { DebtorService } from './debtor.service';
+import { CreateDebtorService } from './services/create-debtor.service';
+import { FindManyDebtorsService } from './services/find-many-debtors.service';
+import { FindOneDebtorService } from './services/find-one-debtor.service';
+import { DeleteDebtorService } from './services/delete-debtor.service';
 
 @Module({
   controllers: [DebtorController],
-  providers: [DebtorService],
+  providers: [
+    DebtorService,
+    CreateDebtorService,
+    FindManyDebtorsService,
+    FindOneDebtorService,
+    DeleteDebtorService,
+  ],
 })
 export class DebtorModule {}
--- src/debtor/debtor.service.ts
+++ src/debtor/debtor.service.ts
@@ -1,4 +1,15 @@
+import { CreateDebtorService } from './services/create-debtor.service';
 import { Injectable } from '@nestjs/common';
+import { FindManyDebtorsService } from './services/find-many-debtors.service';
+import { FindOneDebtorService } from './services/find-one-debtor.service';
+import { DeleteDebtorService } from './services/delete-debtor.service';
 
 @Injectable()
-export class DebtorService {}
+export class DebtorService {
+  constructor(
+    private readonly createDebtorService: CreateDebtorService,
+    private readonly findManyDebtorsService: FindManyDebtorsService,
+    private readonly findOneDebtorService: FindOneDebtorService,
+    private readonly deleteDebtorService: DeleteDebtorService,
+  ) {}
+}
