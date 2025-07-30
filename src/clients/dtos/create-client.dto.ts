0739c933f68caa9e1b20f556558234ebaa81bc26
NunoF <n.fernandes.contact@gmail.com>
2025-07-30 18:01:49 +0200

    refactor(swagger): improve API documentation and property descriptions
    - Remove unused BaseDebtorDto reference in DebtorController
    - Enhance CreateClientDto properties with detailed descriptions, types and examples in Swagger decorators

--- src/clients/dtos/create-client.dto.ts
+++ src/clients/dtos/create-client.dto.ts
@@ -20,56 +20,101 @@ export class CreateClientDto extends OmitType(BaseClientDto, [
   'updatedAt',
   'user',
 ] as const) {
-  @ApiProperty()
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ description: 'Nom du client', type: String, example: 'Doe' })
   readonly name: string;
 
-  @ApiProperty({ type: [String] })
   @IsNotEmpty()
   @IsEmail({}, { each: true })
+  @ApiProperty({
+    description: 'Client emails adresses',
+    type: [String],
+    example: ['example@email.com'],
+  })
   readonly email: string[];
 
-  @ApiPropertyOptional({ type: [String] })
   @IsOptional()
   @IsPhoneNumber(undefined, { each: true })
+  @ApiPropertyOptional({
+    description: 'Client phones numbers',
+    type: [String],
+    example: ['0606060606'],
+  })
   readonly phone?: string[];
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional({
+    description: 'Client address',
+    type: String,
+    example: '123 rue de la Paix',
+  })
   readonly address?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional({
+    description: 'Client city',
+    type: String,
+    example: 'Paris',
+  })
   readonly city?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional({
+    description: 'Client country',
+    type: String,
+    example: 'France',
+  })
   readonly country?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional({
+    description: 'Client zip code',
+    type: String,
+    example: '75000',
+  })
   readonly zipCode?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional({
+    description: 'Client siret number',
+    type: String,
+    example: '12345678901234',
+  })
   readonly siret: string;
 
-  @ApiPropertyOptional({ enum: ['PROFESSIONAL', 'PERSONAL'] })
   @IsOptional()
   @IsEnum(['PROFESSIONAL', 'PERSONAL'])
+  @ApiPropertyOptional({
+    description: 'Client type',
+    type: String,
+    example: ClientType.PROFESSIONAL,
+  })
   readonly type: ClientType;
 
-  @ApiPropertyOptional({ type: [String] })
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Client notes',
+    type: [String],
+    example: ['Note 1', 'Note 2'],
+  })
   readonly notes?: string[];
 
-  @ApiPropertyOptional({ type: 'array', items: { type: 'object' } })
   @IsOptional()
+  @ApiPropertyOptional({
+    description: 'Debort of the client',
+    type: 'array',
+    example: [
+      {
+        id: 'azfezfgsgknsront',
+        name: 'Debort 1',
+      },
+    ],
+  })
   readonly debtor?: any;
 }
--- src/debtor/debtor.controller.ts
+++ src/debtor/debtor.controller.ts
@@ -9,15 +9,12 @@ import {
 import { DebtorService } from './debtor.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateDebtorDto } from './dtos/create-debtor.dto';
-import { ApiExtraModels } from '@nestjs/swagger';
-import { BaseDebtorDto } from './dtos/base-debtor.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('debtors')
 export class DebtorController {
   constructor(private readonly debtorService: DebtorService) {}
 
-  @ApiExtraModels(BaseDebtorDto)
   @Post('new')
   async create(@Body() data: CreateDebtorDto): Promise<CreateDebtorDto> {
     return this.debtorService.create(data);
